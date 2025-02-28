98b61c5cfa8867470c9cee789d7cd143e8a5171c
pablo <pablo.j98@gmail.com>
2025-02-27 21:04:28 -0400

    fix(auth): move main layout to auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,4 @@
-import Header from "@/components/Header";
 import "./globals.css";
-import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Instagram Clone",
@@ -11,11 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-[100vh] flex flex-col justify-between">
-        <Header />
-        <div className="container px-4 mx-auto grow">
           {children}
-        </div>
-        <Footer />
       </body>
     </html>
   );
